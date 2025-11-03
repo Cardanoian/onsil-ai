7e892dd47f1906b29c24f1b61689851ae6a2c9f8
Cardanoian <superiorz1@yu.ac.kr>
2025-11-04 00:29:17 +0900

    close project

--- src/main.tsx
+++
@@ -1,6 +0,0 @@
-import './utils/lamejs-globals'; // lamejs 전역 상수들을 먼저 로드
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
